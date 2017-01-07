1164779e425cba747809a1cf70cf846bc9e594f0
Robin Millette <robin@millette.info>
2017-01-06 22:33:15 -0500

    Remove usd exchange rates.

--- btcaverage.js
+++ btcaverage.js
@@ -2,7 +2,7 @@ var Q = require('q');
 var request = require('request');
 var async = require('async');
 var Promise = require("bluebird");
-var getExchangeRates = require("get-exchange-rates-usd");
+// var getExchangeRates = require("get-exchange-rates-usd");
 var smartaverage = require('smartaverage');
 var providers = require('./providers');
 
@@ -17,18 +17,18 @@ var MINIMUM_VALUES_VARIANCE = 3;
  * @param {String} path
  */
 function findValueByPath(jsonData, path, currency, rate) {
-	var errorParts = false;
-	path.split('.').forEach(function(part) {
-		if (!errorParts) {
-			if (currency) {
-				jsonData = jsonData[part] / rate;
-			} else {
-				jsonData = jsonData[part]
-			}
-			if (!jsonData) errorParts = true;
-		}
-	});
-	return errorParts ? 0 : parseFloat(jsonData);
+  var errorParts = false;
+  path.split('.').forEach(function(part) {
+    if (!errorParts) {
+      if (currency) {
+        jsonData = jsonData[part] / rate;
+      } else {
+        jsonData = jsonData[part]
+      }
+      if (!jsonData) errorParts = true;
+    }
+  });
+  return errorParts ? 0 : parseFloat(jsonData);
 }
 
 /**
@@ -38,22 +38,22 @@ function findValueByPath(jsonData, path, currency, rate) {
  * @param {Function} callback
  */
 function requestPrice(urlAPI, callback) {
-	request({
-		method: 'GET',
-		url: urlAPI,
-		timeout: TIMEOUT,
-		maxRedirects: 2
-	}, function(error, res, body) {
-		if (!error) {
-			try {
-				var current = JSON.parse(body);
-				callback(current);
-			} catch (e) {}
-		}
-		if (!current) {
-			callback({});
-		}
-	});
+  request({
+    method: 'GET',
+    url: urlAPI,
+    timeout: TIMEOUT,
+    maxRedirects: 2
+  }, function(error, res, body) {
+    if (!error) {
+      try {
+        var current = JSON.parse(body);
+        callback(current);
+      } catch (e) {}
+    }
+    if (!current) {
+      callback({});
+    }
+  });
 }
 
 
@@ -61,33 +61,31 @@ function requestPrice(urlAPI, callback) {
  * GET PRICE
  */
 module.exports = function getPrice() {
-	var df = Q.defer();
-	Promise.try(function() {
-		return getExchangeRates();
-	}).then(function(rates) {
-		async.parallel(providers.map(function(provider) {
-				return function(callback) {
-					requestPrice(provider.url, function(jsonResponse) {
-						callback(null, findValueByPath(jsonResponse, provider.path, provider.currency, rates[provider.currency]));
-					});
-				}
-			}),
-			function(err, prices) {
-				var infoAverage = smartaverage(ACCEPTABLE_VARIANCE, MINIMUM_VALUES_VARIANCE, prices);
-				if (infoAverage) {
-					var pricesProviders = {};
-					prices.forEach(function(price, i) {
-						pricesProviders[providers[i].name] = price;
-					});
-					df.resolve({
-						average: infoAverage.average,
-						pricesAverage: infoAverage.dataset,
-						prices: pricesProviders
-					});
-				} else {
-					df.reject(new Error('Was imposible get price average'));
-				}
-			});
-	});
-	return df.promise;
+  var df = Q.defer();
+
+  async.parallel(providers.map(function(provider) {
+      return function(callback) {
+        requestPrice(provider.url, function(jsonResponse) {
+          callback(null, findValueByPath(jsonResponse, provider.path)); //, provider.currency, rates[provider.currency]));
+        });
+      }
+    }),
+    function(err, prices) {
+      var infoAverage = smartaverage(ACCEPTABLE_VARIANCE, MINIMUM_VALUES_VARIANCE, prices);
+      if (infoAverage) {
+        var pricesProviders = {};
+        prices.forEach(function(price, i) {
+          pricesProviders[providers[i].name] = price;
+        });
+        df.resolve({
+          average: infoAverage.average,
+          pricesAverage: infoAverage.dataset,
+          prices: pricesProviders
+        });
+      } else {
+        df.reject(new Error('Was imposible get price average'));
+      }
+    }
+  );
+  return df.promise;
 };
